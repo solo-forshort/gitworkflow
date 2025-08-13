354baa03ea40f965712b3283e6783f98eeee16b2
Solomon Asare <solomonasare0551.com>
2025-08-13 13:59:58 +0000

    Completed arithmetic of all Functions
    Added subtreactionFunction
    added multiplyFunction
    added devideFunction

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
 def addFunction(x, y):
     return x + y
+
+def subtractFunction(x, y):
+    return x - y
+
+def multiplyFunction(x, y):
+    return x * y
+
+def divideFunction(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Division by zero error"
+    
+
